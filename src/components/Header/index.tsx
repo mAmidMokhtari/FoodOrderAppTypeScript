18850b8951387390f68dd0097b89966aa4779a74
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-08-10 17:35:21 +0330

    TR-1: implement Header cmp - Header Cmp implemented - Styles updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Cart from "./Cart";
+import styles from "./styles.module.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <h1>Food Order App</h1>
+        </div>
+        <Cart cartCounter={3} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
